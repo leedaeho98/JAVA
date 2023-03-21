69355fb64841ea76547d2de5520c96afa617a82e
이대호 <117264484+leedaeho98@users.noreply.github.com>
2023-03-21 17:47:24 +0900

    Delete academy/board directory

--- academy/board/src/main/java/com/yangdoll/board/servic/Action.java
+++
@@ -1,9 +0,0 @@
-package com.yangdoll.board.servic;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Action {
-	// 모든 메소드에서 execute를 실행하도록 만들기 위해서
-	ActionForward  execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
